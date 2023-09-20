ea303779035bbfa2c3ce06387b0669bdcd2d56be
suanebc <60412898+suanebc@users.noreply.github.com>
2023-09-20 10:51:10 -0300

    Projeto - fase 1: criação da funcionalidade ordenação

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ppads-tech-em-acao
 PRATICA PROFISSIONAL EM ANALISE E DESENVOLVIMENTO DE SISTEMAS {TURMA 05A} 2023/2
+
+
+para rodar o projeto em sua máquina digite no terminal : npm run dev
--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   const [filter, setFilter] = useState("All");
 
-  const [sorter, setSorter] = useState("Asc");
+  const [sort, setSort] = useState("Asc");
 
   const addToDo = (text, category) => {
     const newToDos = [... toDos, {
@@ -70,7 +70,9 @@ function App() {
 
       <div className='list'>
         <Search search={search} setSearch={setSearch} />
-        <Filter filter={filter} setFilter={setFilter} />
+
+        <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+
         <div className='toDo-list'>
           {toDos
           .filter((todo) => 
@@ -81,10 +83,19 @@ function App() {
           : !todo.isCompleted
           )
           .filter((todo) =>
-          todo.text.toLowerCase().includes(search.toLowerCase())
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) => 
+            sort === "Asc" 
+              ? a.text.localeCompare(b.text) 
+              : b.text.localeCompare(a.text)
           )
           .map((todo) =>(
-          <List key={todo.id} todo={todo} removeToDo={removeToDo} completeToDo={completeToDo} />
+            <List 
+              key={todo.id} 
+              todo={todo} 
+              removeToDo={removeToDo} 
+              completeToDo={completeToDo} />
           ))}
         </div>
       </div>
